cdf1fe15f39a99f453a86f2d55979424cd5a9d6f
rav130 <kau14941@sheridancollege.ca>
2023-05-25 14:30:58 -0400

    student details version 2

---
+++ src/week3/Week3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week3;
+
+public class Week3 {
+//Push
+//        Initialize the versioning
+//    .gitconfig
+//    .gitignore
+//            local repository
+//                    commit
+   
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
